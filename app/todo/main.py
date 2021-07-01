556941897a000e43daef1c45cf3e1f6ef3a7219b
Haafiz <kaasib@gmail.com>
2021-07-01 22:10:42 +0500

    fixed response validation

--- app/todo/main.py
+++ app/todo/main.py
@@ -22,13 +22,13 @@ def get_welcome():
     return {"detail": "Welcome to To Do items, try /todo_items CRUD"}
 
 
-@app.get("/todo_items", response_model=List[schemas.TodoItemResponse])
+@app.get("/todo_items")
 def get_all_todo_items(db: Session = Depends(get_db)):
     todo_items = db.query(models.TodoItem).all()
     return todo_items
 
 
-@app.get("/todo_items/{item_id}", response_model=schemas.TodoItemResponse)
+@app.get("/todo_items/{item_id}")
 def get_todo_item(item_id: int, response: Response, db: Session = Depends(get_db)):
     todo_item = db.query(models.TodoItem).filter(models.TodoItem.id == item_id).first()
     if not todo_item:
@@ -37,7 +37,7 @@ def get_todo_item(item_id: int, response: Response, db: Session = Depends(get_db
     return todo_item
 
 
-@app.post("/todo_items", status_code=status.HTTP_201_CREATED, response_model=schemas.TodoItemResponse)
+@app.post("/todo_items", status_code=status.HTTP_201_CREATED)
 def create_todo_item(request: schemas.TodoItem, db: Session = Depends(get_db)):
     new_todo_item = models.TodoItem(title=request.title, description=request.description)
     db.add(new_todo_item)
@@ -47,22 +47,21 @@ def create_todo_item(request: schemas.TodoItem, db: Session = Depends(get_db)):
     return new_todo_item
 
 
-@app.put("/todo_items/{item_id}", response_model=schemas.TodoItemResponse)
+@app.put("/todo_items/{item_id}")
 def update_todo_item(item_id: int, request: schemas.TodoItem, db: Session = Depends(get_db)):
-    todo_item = db.query(models.TodoItem).filter(models.TodoItem.id == item_id).first()
-    if not todo_item:
+    todo_item = db.query(models.TodoItem).filter(models.TodoItem.id == item_id)
+    if not todo_item.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This todo item doesn't exist")
     todo_item.update({'title': request.title, 'description': request.description}, synchronize_session=False)
     db.commit()
-    db.refresh(todo_item)
 
-    return todo_item
+    return todo_item.first()
 
 
 @app.delete("/todo_items/{item_id}")
 def delete_todo_item(item_id: int,  db: Session = Depends(get_db)):
-    todo_item = db.query(models.TodoItem).filter(models.TodoItem.id == item_id).first()
-    if not todo_item:
+    todo_item = db.query(models.TodoItem).filter(models.TodoItem.id == item_id)
+    if not todo_item.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This todo item doesn't exist")
     todo_item.delete(synchronize_session=False)
     db.commit()
--- app/todo/schemas.py
+++ app/todo/schemas.py
@@ -4,9 +4,3 @@ from pydantic import BaseModel
 class TodoItem(BaseModel):
     title: str
     description: str
-
-
-class TodoItemResponse(BaseModel):
-    title: str
-    description: str
-    id: int
